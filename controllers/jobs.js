8d124cea9c256e1c6e38f4b8a82a32e037edf709
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-09-30 20:00:07 -0400

    feat: implement create job button and render in jobs list

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -9,9 +9,10 @@ const createJob = (req, res) => {
   res.send("<h1>POST /jobs</h1><p>Add a new job listing</p>");
 };
 const getNewJobForm = (req, res) => {
-  res.send(
-    "<h1>GET /jobs/new</h1><p>Put up the form to create a new entry</p>"
-  );
+  // res.send(
+  //   "<h1>GET /jobs/new</h1><p>Put up the form to create a new entry</p>"
+  // );
+  res.render("jobs/new");
 };
 const getEditJobForm = (req, res) => {
   const { id } = req.params;
